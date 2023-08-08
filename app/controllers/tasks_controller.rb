6b5bec9b606e20a3fcd9bf31dac1cb900bb46ae4
alan tecuapetla <alan.tecuapetla@gmail.com>
2023-08-08 16:41:47 +0900

    Super done

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,51 @@
 class TasksController < ApplicationController
+  #READ all
+  def index
+    @tasks = Task.all
+  end
+
+  #READ one
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  #CREATE 1)
+  def new
+    @task = Task.new
+  end
+  #CREATE 2)
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to task_path(@task)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  #UPDATE
+  def edit
+    @task = Task.find(params[:id])
+  end
+  def update
+    @task = Task.find(params[:id])
+    if @task.update(task_params)
+      redirect_to task_path(@task)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  #DESTROY
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path, status: :see_other
+  end
+
+  private
+  #SQL SAFETY MEASURES
+  def task_params
+    params.require(:task).permit(:title, :details)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,19 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # http_verb '/path', to: 'controller#action', as: :prefix
+  # CRUD: READ all, CREATE one, READ one, UPDATE one, DESTROY one
+  # READ all
+  get '/index', to: 'tasks#index', as: :tasks
+  # CREATE 1)create a page for the form (GET user input) 2) send the info(POST)
+  get '/new', to: 'tasks#new', as: :new_task
+  post '/index', to: 'tasks#create'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # READ
+  get '/tasks/:id', to: 'tasks#show', as: :task
+
+  # UPDATE
+  get '/tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch 'tasks/:id', to: 'tasks#update'
+
+  # DESTROY
+  delete '/tasks/:id', to: 'tasks#destroy'
 end
